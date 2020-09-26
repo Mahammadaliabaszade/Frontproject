2d1739d3eec8deab4efeaebc6e0aff83e02838d4
Mahammadaliabaszade <68659317+Mahammadaliabaszade@users.noreply.github.com>
2020-09-26 21:17:28 +0400

    grid3 yarisi hazir

--- js/home.js
+++ js/home.js
@@ -1,6 +1,25 @@
-  // function onClickMenu(){
-  // 	document.getElementById("menu").classList.toggle("change");
-  // 	document.getElementById("nav").classList.toggle("change");
+  function onClickMenu() {
+      document.getElementById("menu").classList.toggle("change");
+      document.getElementById("nav").classList.toggle("change");
 
-  // 	document.getElementById("menu-bg").classList.toggle("change-bg");
-  // }
+      document.getElementById("menu-bg").classList.toggle("change-bg");
+  }
+
+  $(document).ready(function() {
+
+      $(window).scroll(function() {
+          var navbar = $('header nav '),
+              navHeight = navbar.height(),
+              scrollPos = $(this).scrollTop();
+
+          if (scrollPos > navHeight) {
+              navbar.addClass('fixed');
+              $('header').css('padding-top', navHeight + 'px')
+          } else {
+              navbar.removeClass('fixed');
+              $('header').css('padding-top', '0')
+          }
+
+      })
+
+  })
