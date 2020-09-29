5879d038bd12dccb49141991faebc043b31a1bdb
Mahammadaliabaszade <68659317+Mahammadaliabaszade@users.noreply.github.com>
2020-09-29 21:34:28 +0400

    ilk commit

---
+++ js/dist/page5.dev.js
@@ -0,0 +1,23 @@
+"use strict";
+
+function onClickMenu() {
+  document.getElementById("menu").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
+  document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var navbar = $('header nav '),
+        navHeight = navbar.height(),
+        scrollPos = $(this).scrollTop();
+
+    if (scrollPos > navHeight) {
+      navbar.addClass('fixed');
+      $('header').css('padding-top', navHeight + 'px');
+    } else {
+      navbar.removeClass('fixed');
+      $('header').css('padding-top', '0');
+    }
+  });
+});
